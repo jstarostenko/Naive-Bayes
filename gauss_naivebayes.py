2494440cd3a874b381179c72a595f3ad783d59ff
Julia S <j.starostenko@gmail.com>
2016-05-07 11:10:44 -0400

    Update

--- gauss_naivebayes.py
+++ gauss_naivebayes.py
@@ -2,6 +2,6 @@ from sklearn import datasets
 from sklearn.naive_bayes import GaussianNB
 
 def gauss(X,y):
-	clf = GaussianNB()
-	clf.fit(X,y)
-	return clf
+    clf = GaussianNB()
+    clf.fit(X,y)
+    return clf
